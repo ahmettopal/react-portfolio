dba19824b98c4cee6aa57b1e4b7d23f62f460024
Ahmet Topal <37218739+ahmettopal@users.noreply.github.com>
2022-10-04 12:27:36 +0300

    add navbar & content body

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -11,11 +17,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to My site
-        </h1>
-
+      <main className='bg-white px-10'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>Ahmet</h1>
+            <ul className='flex items-center' >
+              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>
+              <li className='bg-cyan-500 text-white px-4 py-2 rounded-md ml-8'>Resume</li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className="text-5xl py-2">Ahmet Topal</h2>
+            <h3 className="text-2xl py-2">Full Stack Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">Hello Im Full stack Developer</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+        </section>
 
       </main>
 
