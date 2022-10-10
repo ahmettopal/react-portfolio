c74c1750158aae02dd3eb6f7a577bd561326e8b4
Ahmet Topal <37218739+ahmettopal@users.noreply.github.com>
2022-10-10 11:15:10 +0300

    update greetings page

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
+export { default as Greetings } from './greeting/Greeting';
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-} from "react-icons/ai";
-import { Navbar, Footer } from '../components';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { Navbar, Footer, Greetings } from '../components';
 
 export default function Home() {
   return (
@@ -19,20 +15,7 @@ export default function Home() {
       <main className='bg-white md:px-5 xl:px-20'>
         <section className='md:min-h-screen 2xl:min-h-full'>
           <Navbar />
-          <div className='flex items-center justify-center flex-col-reverse md:flex-row'>
-            <div className='text-center p-10'>
-              <h2 className="text-4xl md:text-5xl 2xl:text-7xl py-2">Ahmet Topal</h2>
-              <h3 className="text-xl md:text-3xl 2xl:text-4xl py-2">Full Stack Developer</h3>
-              <p className="text-md 2xl:text-lg py-5 leading-8 text-gray-1000">Hello Im Full stack Developer</p>
-            </div>
-            <div className="rounded-full w-40 h-40 relative overflow-hidden md:h-56 md:w-56 2xl:h-72 2xl:w-72 bg-slate-600">
-              <Image src="https://picsum.photos/200" layout="fill" objectFit="cover" />
-            </div>
-          </div>
-          <div className="text-5xl 2xl:text-7xl flex justify-center gap-16 py-3 2xl:py-10 text-gray-600">
-            <AiFillLinkedin />
-            <a href='https://github.com/ahmettopal' target="_blank"><AiFillGithub /></a>
-          </div>
+          <Greetings />
         </section>
         <section>
           <div className="flex justify-center">
