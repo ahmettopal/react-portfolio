16adfc8fb5536d11206a1f42352bc4e39380caf1
Ahmet Topal <37218739+ahmettopal@users.noreply.github.com>
2022-10-12 11:25:29 +0300

    feat dark mode

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Footer = ({ ...props }) => {
     return (
-        <footer className="flex-1 flex p-5 justify-center mt-5 border-gray-100 border-t-2">
-            <h4>
-                © {new Date().getFullYear()} Powered by{' '} Ahmet Topal
-            </h4>
-        </footer>
+        <div className="dark:bg-slate-600">
+            <footer className="flex-1 flex p-5 justify-center mt-5 border-gray-100 border-t-2">
+                <h4>
+                    © {new Date().getFullYear()} Powered by{' '} Ahmet Topal
+                </h4>
+            </footer>
+        </div>
     );
 };
 
