a15063eb698cc2e0de152f60c64d3d1097972c6d
Ahmet Topal <37218739+ahmettopal@users.noreply.github.com>
2023-02-26 18:28:10 +0300

    add portfolio page

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,22 +14,27 @@ export const Navbar = ({ onMode = () => { }, darkMode }) => {
                         <BsFillMoonFill className='cursor-pointer text-2xl' />}
                 </li>
                 <li className='bg-white dark:bg-zinc-700 text-black dark:text-white px-2 py-2 rounded-md ml-3 shadow-md'>
-                    <a
-                        href={resume}
-                        alt="resume"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Resume</a>
-                </li>
-                <li className='bg-white dark:bg-zinc-700 text-black dark:text-white px-2 py-2 rounded-md ml-3 shadow-md'>
                     <NextLink href={"/"}>
                         Home
+                    </NextLink>
+                </li>
+                <li className='bg-white dark:bg-zinc-700 text-black dark:text-white px-2 py-2 rounded-md ml-3 shadow-md'>
+                    <NextLink href={"/portfolio"}>
+                        Portfolio
                     </NextLink>
                 </li>
                 <li className='bg-white dark:bg-zinc-700 text-black dark:text-white px-2 py-2 rounded-md ml-3 shadow-md'>
                     <NextLink href={"/contact"}>
                         Contact
                     </NextLink>
+                </li>
+                <li className='bg-white dark:bg-zinc-700 text-black dark:text-white px-2 py-2 rounded-md ml-3 shadow-md'>
+                    <a
+                        href={resume}
+                        alt="resume"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >Resume</a>
                 </li>
             </ul>
         </nav>
---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const portfolio = () => {
+    return (
+        <div className="dark:bg-zinc-700 w-screen">
+            <p className='text-center'>Portfolio</p>
+        </div>
+    )
+}
+
+export default portfolio
